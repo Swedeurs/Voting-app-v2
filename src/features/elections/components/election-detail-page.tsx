b3841a6fcdcc2eed5f2f70b1a6149416db9cce76
Leo Swedérus <leo.swederus@appliedtechnology.se>
2025-01-09 23:13:44 +0100

    check for isnan

--- src/features/elections/components/election-detail-page.tsx
+++ src/features/elections/components/election-detail-page.tsx
@@ -2,13 +2,17 @@ import TopNav from "@/components/top-nav";
 import { ElectionDetailContainer } from "@/features/elections/components/election-detail-container";
 import ElectionsList from "./elections-list";
 
-
 type Props = {
   params: { id: string };
 };
 
 export default function ElectionDetailPage({ params }: Props) {
-  const electionId = Number(params.id);
+  const electionId = parseInt(params.id, 10); 
+
+  if (isNaN(electionId)) {
+
+    return <div>Invalid election ID</div>;
+  }
 
   return (
     <>
