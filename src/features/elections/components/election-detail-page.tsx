b01241b79ebb046c2433f8c129192a8ce35473fc
Leo Swedérus <leo.swederus@appliedtechnology.se>
2025-01-09 22:53:18 +0100

    thining out pages

--- src/features/elections/components/election-detail-page.tsx
+++ src/features/elections/components/election-detail-page.tsx
@@ -1,60 +1,19 @@
 import TopNav from "@/components/top-nav";
-import { ElectionDetail } from "@/features/elections/components/detail-page";
-import ElectionsList from "@/features/elections/components/elections-list";
-import { electionService } from "..";
+import { ElectionDetailContainer } from "@/features/elections/components/election-detail-container";
+import ElectionsList from "./elections-list";
 
 
 type Props = {
   params: { id: string };
 };
 
-export default async function ElectionDetailPage({ params }: Props) {
-  const id = Number(params.id);
-
-  const election = await electionService.getElectionById(id);
-
-  if (!election) {
-    return (
-      <div className="text-center">
-        <h1 className="text-xl font-bold">Election Not Found</h1>
-        <p>Please check the election ID.</p>
-      </div>
-    );
-  }
-
-  const representatives =
-    await electionService.getRepresentativesByElectionId(id);
-
-  const alternatives = JSON.parse(election.alternatives || "[]");
-
-  const initialVotes: {
-    representatives: Record<number, number>;
-    alternatives: Record<string, number>;
-  } = {
-    representatives: representatives.reduce(
-      (acc, rep) => {
-        acc[rep.id] = Math.floor(Math.random() * 10) + 1;
-        return acc;
-      },
-      {} as Record<number, number>,
-    ),
-    alternatives: alternatives.reduce(
-      (acc, alt) => {
-        acc[alt] = Math.floor(Math.random() * 10) + 1;
-        return acc;
-      },
-      {} as Record<string, number>,
-    ),
-  };
+export default function ElectionDetailPage({ params }: Props) {
+  const electionId = Number(params.id);
 
   return (
     <>
       <TopNav />
-      <ElectionDetail
-        election={election}
-        representatives={representatives}
-        initialVotes={initialVotes}
-      />
+      <ElectionDetailContainer electionId={electionId} />
       <ElectionsList />
     </>
   );
